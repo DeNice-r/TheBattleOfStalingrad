087e4b09e519f0c0a12f66d15539e51a00087d3b
DeNice-r <kdenis0610@gmail.com>
2020-10-12 20:50:26 +0300

    Initial commit

---
+++ Assets/TankScript.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+public class TankScript : MonoBehaviour
+{
+
+    /////*******************************************/////
+    /////                   VARS                    /////  
+    /////*******************************************/////
+
+    public TrackScript trackLeft;
+    public TrackScript trackRight;
+
+    public KeyCode keyMoveForward;
+    public KeyCode keyMoveReverse;
+    public KeyCode keyRotateRight;
+    public KeyCode keyRotateLeft;
+
+    bool moveForward = false;
+    bool moveReverse = false;
+    float moveSpeed = 3f;
+    float moveSpeedReverse = 0f;
+    float moveAcceleration = 1.5f;
+    float moveDeceleration = 3f;
+    float moveSpeedMax = 20f;
+
+    bool rotateRight = false;
+    bool rotateLeft = false;
+    float rotateSpeedRight = 0f;
+    float rotateSpeedLeft = 0f;
+    float rotateAcceleration = 4f;
+    float rotateDeceleration = 10f;
+    float rotateSpeedMax = 130f;
+
+
+    /////*******************************************/////
+    /////                 UPDATE                    /////  
+    /////*******************************************/////
+
+    void Update()
+    {
+        rotateLeft = (Input.GetKeyDown(keyRotateLeft)) ? true : rotateLeft;
+        rotateLeft = (Input.GetKeyUp(keyRotateLeft)) ? false : rotateLeft;
+        if (rotateLeft)
+        {
+            rotateSpeedLeft = (rotateSpeedLeft < rotateSpeedMax) ? rotateSpeedLeft + rotateAcceleration : rotateSpeedMax; } else { rotateSpeedLeft = (rotateSpeedLeft > 0) ? rotateSpeedLeft - rotateDeceleration : 0;
+        }
+        transform.Rotate(0f, 0f, rotateSpeedLeft * Time.deltaTime);
+
+        rotateRight = (Input.GetKeyDown(keyRotateRight)) ? true : rotateRight;
+        rotateRight = (Input.GetKeyUp(keyRotateRight)) ? false : rotateRight;
+        if (rotateRight)
+        {
+            rotateSpeedRight = (rotateSpeedRight < rotateSpeedMax) ? rotateSpeedRight + rotateAcceleration : rotateSpeedMax; } else { rotateSpeedRight = (rotateSpeedRight > 0) ? rotateSpeedRight - rotateDeceleration : 0;
+        }
+        transform.Rotate(0f, 0f, rotateSpeedRight * Time.deltaTime * -1f);
+
+        moveForward = (Input.GetKeyDown(keyMoveForward)) ? true : moveForward;
+        moveForward = (Input.GetKeyUp(keyMoveForward)) ? false : moveForward;
+        if (moveForward)
+        {
+            moveSpeed = (moveSpeed < moveSpeedMax) ? moveSpeed + moveAcceleration : moveSpeedMax; } else { moveSpeed = (moveSpeed > 0) ? moveSpeed - moveDeceleration : 0;
+        }
+        transform.Translate(0f, moveSpeed * Time.deltaTime, 0f);
+
+        moveReverse = (Input.GetKeyDown(keyMoveReverse)) ? true : moveReverse;
+        moveReverse = (Input.GetKeyUp(keyMoveReverse)) ? false : moveReverse;
+        if (moveReverse)
+        {
+            moveSpeedReverse = (moveSpeedReverse < moveSpeedMax) ? moveSpeedReverse + moveAcceleration : moveSpeedMax; } else { moveSpeedReverse = (moveSpeedReverse > 0) ? moveSpeedReverse - moveDeceleration : 0;
+        }
+        transform.Translate(0f, moveSpeedReverse * Time.deltaTime * -1f, 0f);
+
+        if (moveForward | moveReverse | rotateRight | rotateLeft)
+        {
+            trackStart();
+        }
+        else
+        {
+            trackStop();
+        }
+
+    }
+
+    /////*******************************************/////
+    /////                METHODS                    /////  
+    /////*******************************************/////
+
+    void trackStart()
+    {
+        trackLeft.animator.SetBool("isMoving", true);
+        trackRight.animator.SetBool("isMoving", true);
+    }
+
+    void trackStop()
+    {
+        trackLeft.animator.SetBool("isMoving", false);
+        trackRight.animator.SetBool("isMoving", false);
+    }
+
+}
+
